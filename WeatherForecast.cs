093ff498b9f54a66567c3b145184711d573a468b
unknown <qhikizaalicia@gmail.com>
2024-05-02 09:55:43 +0700

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Jurnal10_1302220011_KPL
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
